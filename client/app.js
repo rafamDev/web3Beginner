617ee8c5c1bc15121c14f2ce459f836f8b62f4b3
rafamDev <76625223+rafamDev@users.noreply.github.com>
2021-12-29 14:24:55 +0100

    almost done it

--- client/app.js
+++ client/app.js
@@ -1,7 +1,9 @@
 App = {
+    contracts: {},
     init: () => {
         console.log("App loaded")
         App.loadEthereum()
+        App.loadContracts()
     },
     loadEthereum: async () => {
         //Metamask library.
@@ -16,6 +18,16 @@ App = {
         }else{
             alert("No ethereum browser is installed. Try installing Metamask")
         }
+    }, 
+    
+    loadContracts: async () => {
+        const response = await fetch("MyTasksContract.json")
+        const taskContractJson = await response.json()
+         App.contracts.taskContract = TruffleContract(taskContractJson); //Solidity contract
+         App.contracts.taskContract.setProvider(App.web3Provider);
+         //Our account is connected from metamask.
+           App.taskContract = await App.contracts.taskContract.deplyed()
+           //Contract deployed in eth network.
     } 
 }
 
